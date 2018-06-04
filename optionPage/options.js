82e3f442cce6e35b42075711e1a746407f2fb23a
sukesan-jp <35905884+sukesan-jp@users.noreply.github.com>
2018-06-04 22:37:06 +0900

    Update options.js

--- optionPage/options.js
+++ optionPage/options.js
@@ -1,4 +1,11 @@
 
+function init(object) {
+  const initOM = document.querySelector('input[value="'+object.openMethod+'"]');
+  initOM.checked = true;
+  const initLC = document.querySelector('option[value="'+object.languageCode+'"]');
+  initLC.selected = true;
+}
+
 function clearNotice(ID) {
   browser.notifications.clear(ID);
 }
@@ -8,9 +15,9 @@ function saveHTO() {
 
   browser.storage.local.set({openMethod: valueOM});
   browser.notifications.create('noticeOM', {
-    'type': 'basic',
-    'title': '"Quick translate from context menu"',
-    'message': 'Setting is changed. Open by new "'+valueOM+'"'
+    'type'   : 'basic',
+    'title'  : '"Quick translate from context menu".',
+    'message': 'Setting is changed. Open by new "'+valueOM+'".'
   });
   setTimeout(function(){ clearNotice('noticeOM') }, 4000);
 }
@@ -20,12 +27,15 @@ function saveLOTTD() {
 
   browser.storage.local.set({languageCode: valueLC});
   browser.notifications.create('noticeLC', {
-    'type': 'basic',
-    'title': '"Quick translate from context menu"',
-    'message': 'Setting is changed. Current language code is "'+valueLC+'"'
+    'type'   : 'basic',
+    'title'  : '"Quick translate from context menu".',
+    'message': 'Setting is changed. Current language code is "'+valueLC+'".'
   });
   setTimeout(function(){ clearNotice('noticeLC') }, 4000);
 }
+
+const initsetting = browser.storage.local.get(['openMethod', 'languageCode'])
+  .then(init);
 
 document.querySelector('#HTO').addEventListener('input', saveHTO);
 document.querySelector('#LOTTD').addEventListener('input', saveLOTTD);
