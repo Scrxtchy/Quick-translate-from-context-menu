a63532fe86bfb1333f2eb1a57d0bc6ceb93bb8d8
sukesan-jp <35905884+sukesan-jp@users.noreply.github.com>
2018-06-09 22:01:44 +0900

    Update addMenu01.js

--- addMenu01.js
+++ addMenu01.js
@@ -7,60 +7,60 @@ const ID02 = 'clickPage';
 const title02 = 'Translate this page';
 
 // menu
-browser.menus.create({
+const menu01 = browser.menus.create({
   id: ID01, title: title01, contexts: ['selection'],
 });
 
-browser.menus.create({
+const menu02 = browser.menus.create({
   id: ID02, title: title02, contexts: ['page'],
 });
 
 // behavior
-browser.menus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case ID01:
-      function Language01(object) {
-        function winOrTab01(object) {
-          let openMethod = 'window';
-          if (object.openMethod) {
-            openMethod = object.openMethod;
-          }
-          switch (openMethod) {
-            case 'window':
-              const createData01 = {height: 720, width: 800, url: URL01};
-              browser.windows.create(createData01);
-              break;
-            case 'tab':
-              browser.tabs.create({url: URL01});
-              break;
-          }
-        }
-        let languageCode01 = 'en';
-        if (object.languageCode) {
-          languageCode01 = object.languageCode;
-        }
-        const URL01 = 'https://translate.google.com/#auto/'+languageCode01+'/'+info.selectionText
-                      .replace(/\%/g, '%25').replace(/\//g, '%2F');
-        const getOpenMethod = browser.storage.local.get('openMethod')
-          .then(winOrTab01);
-      }
-      const getLanguageCode01 = browser.storage.local.get('languageCode')
-        .then(Language01);
-      break;
+function openByNewWindow(URL) {
+  browser.windows.create({height: 720, width: 800, url: URL});
+}
 
-    case ID02:
-      function Language02(object) {
-        let languageCode02 = 'en';
-        if (object.languageCode) {
-          languageCode02 = object.languageCode;
+function openByNewTab(URL) {
+  browser.tabs.create({url: URL});
+}
+
+function noticeNotSet() {
+  browser.notifications.create('noticeNS', {
+    'type'   : 'basic',
+    'title'  : '"Quick translate from context menu".',
+    'message': 'Setting is not completed. Please make settings on the add-on\'s details page. '
+  });
+  setTimeout(function(){ browser.notifications.clear('noticeNS'); }, 6000);
+}
+
+browser.menus.onClicked.addListener((info, tab) => {
+  function menuBehavior(obj) {
+    if ((obj.textOpenMethod == null) || (obj.languageCode == null)) {
+      noticeNotSet();
+    }
+    const objLC = obj.languageCode;
+
+    switch (info.menuItemId) {
+      case ID01:
+        const URL01 = 'https://translate.google.com/#auto/'+objLC+'/'+info.selectionText
+                     .replace(/\%/g, '%25').replace(/\//g, '%2F');
+        switch (obj.textOpenMethod) {
+          case 'window':
+            openByNewWindow(URL01);
+            break;
+          case 'tab':
+            openByNewTab(URL01);
+            break;
         }
-        const URL02 = 'https://translate.google.com/translate?hl='+
-                      languageCode02+'&sl=auto&tl='+languageCode02+'&u='+info.pageUrl;
-        const createData02 = {url: URL02};
-        browser.tabs.create(createData02);
-      }
-      const getLanguageCode02 = browser.storage.local.get('languageCode')
-        .then(Language02);
-      break;
+        break;
+      case ID02:
+        const URL02 = 'https://translate.google.com/translate?hl='+objLC+'&sl=auto&tl='
+                     +objLC+'&u='+info.pageUrl;
+        openByNewTab(URL02);
+        break;
+    }
   }
+
+  const getMenuInfo = browser.storage.local.get(['textOpenMethod', 'languageCode'])
+    .then(menuBehavior);
 });
