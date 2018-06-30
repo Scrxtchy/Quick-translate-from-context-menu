dbe5348d60e3f8511529f1e3798ced5cae44a3f8
sukesan-jp <35905884+sukesan-jp@users.noreply.github.com>
2018-06-30 16:00:33 +0900

    ver.1.1.0

--- optionPage/options.js
+++ optionPage/options.js
@@ -1,41 +1,45 @@
 
-function init(object) {
-  const initOM = document.querySelector('input[value="'+object.textOpenMethod+'"]');
-  initOM.checked = true;
-  const initLC = document.querySelector('option[value="'+object.languageCode+'"]');
-  initLC.selected = true;
+function init(obj) {
+  const initOM_text    = document.querySelector('#formOM_text input[value="'+obj.openMethod_text+'"]');
+  const initOM_website = document.querySelector('#formOM_website input[value="'+obj.openMethod_website+'"]');
+  const initLC         = document.querySelector('option[value="'+obj.languageCode+'"]');
+
+  initOM_text.checked    = true;
+  initOM_website.checked = true;
+  initLC.selected        = true;
 }
 
-function clearNotice(ID) {
-  browser.notifications.clear(ID);
+function saveOM_text() {
+  const valueOM_text = document.querySelector('input[name="howToOpenText"]:checked').value;
+  browser.storage.local.set({ openMethod_text: valueOM_text });
 }
 
-function saveHTO() {
-  const valueTextOM = document.querySelector('input[name="howToOpen"]:checked').value;
-
-  browser.storage.local.set({textOpenMethod: valueTextOM});
-  browser.notifications.create('noticeOM', {
-    'type'   : 'basic',
-    'title'  : '"Quick translate from context menu".',
-    'message': 'Setting is changed. Open by new "'+valueTextOM+'".'
-  });
-  setTimeout(function(){ clearNotice('noticeOM') }, 4000);
+function saveOM_website() {
+  const valueOM_website = document.querySelector('input[name="howToOpenWebsite"]:checked').value;
+  browser.storage.local.set({ openMethod_website: valueOM_website });
 }
 
-function saveLOTTD() {
+function saveLC() {
   const valueLC = document.querySelector('#languageCode').value;
-
-  browser.storage.local.set({languageCode: valueLC});
-  browser.notifications.create('noticeLC', {
-    'type'   : 'basic',
-    'title'  : '"Quick translate from context menu".',
-    'message': 'Setting is changed. Current language code is "'+valueLC+'".'
-  });
-  setTimeout(function(){ clearNotice('noticeLC') }, 4000);
+  browser.storage.local.set({ languageCode: valueLC });
 }
 
-const initSetting = browser.storage.local.get(['textOpenMethod', 'languageCode'])
+const initSetting = browser.storage.local.get(['openMethod_text', 'openMethod_website', 'languageCode'])
   .then(init);
 
-document.querySelector('#HTO').addEventListener('input', saveHTO);
-document.querySelector('#LOTTD').addEventListener('input', saveLOTTD);
+// Update processing
+document.getElementById('formOM_text').addEventListener('input', saveOM_text);
+document.getElementById('formOM_website').addEventListener('input', saveOM_website);
+document.getElementById('formLC').addEventListener('input', saveLC);
+
+// For multilingual
+document.getElementById('divBehaviorWhen').textContent        = browser.i18n.getMessage('optionpageBehaviorWhen');
+document.getElementById('strongCaseOfText').textContent       = browser.i18n.getMessage('optionpageCaseOfText');
+document.getElementById('strongCaseOfWebsite').textContent    = browser.i18n.getMessage('optionpageCaseOfWebsite');
+document.getElementById('labelOpenByNewTab_A').textContent    = browser.i18n.getMessage('optionpageOpenByNewTab');
+document.getElementById('labelOpenByNewWindow_A').textContent = browser.i18n.getMessage('optionpageOpenByNewWindow');
+document.getElementById('labelOpenByNewTab_B').textContent    = browser.i18n.getMessage('optionpageOpenByNewTab');
+document.getElementById('labelOpenByNewWindow_B').textContent = browser.i18n.getMessage('optionpageOpenByNewWindow');
+
+document.getElementById('divLanguageOf').textContent = browser.i18n.getMessage('optionpageLanguageOf');
+document.getElementById('pIfSetToEn').textContent      = browser.i18n.getMessage('optionpageIfSetEn');
